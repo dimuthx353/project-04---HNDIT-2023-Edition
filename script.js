e95f852b8562159f960acf880e44e8b20818b79d
Dimuth Adithya <dimuthx353@gmail.com>
2024-02-01 19:56:57 +0530

    done

---
+++ script.js
@@ -0,0 +1,38 @@
+import { members } from './members.js';
+
+const container = document.getElementById('container');
+const downBtn = document.getElementById('downBtn');
+const topBtn = document.getElementById('topBtn');
+
+members.students.forEach((element) => {
+  const newDiv = document.createElement('div');
+  const imgEl = document.createElement('img');
+
+  imgEl.setAttribute('src', `./images/${element.indexNumber}.jpg`);
+
+  imgEl.addEventListener('error', function () {
+    this.setAttribute('src', './images/none.jpg');
+    console.clear();
+  });
+
+  newDiv.innerHTML = `
+        <div class="info">
+        <p class="index" id="index"> <span>KEG/IT/2022/${element.indexNumber}</span></p>
+        <p class="name" id="name"><span>${element.name}</span></p>
+        <p class="gender" id="gender"><span>${element.gender}</span></p>
+        </div>
+    `;
+  newDiv.prepend(imgEl);
+
+  console.log(newDiv);
+
+  container.appendChild(newDiv);
+});
+
+downBtn.addEventListener('click', (e) => {
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+topBtn.addEventListener('click', () => {
+  window.scrollTo(0, 0);
+});
